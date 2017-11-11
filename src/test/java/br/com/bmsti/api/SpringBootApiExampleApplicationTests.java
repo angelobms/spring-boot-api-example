f67ee8c8fc0fdef603142f77dbc48a2d53ef8102
Angelo Brandão <angelobms@gmail.com>
2017-11-11 16:38:52 -0300

    Configured profiles.

--- src/test/java/br/com/bmsti/api/SpringBootApiExampleApplicationTests.java
+++ src/test/java/br/com/bmsti/api/SpringBootApiExampleApplicationTests.java
@@ -1,16 +1,25 @@
 package br.com.bmsti.api;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class SpringBootApiExampleApplicationTests {
+	
+	@Value("${pagination_qtd_per_page}")
+	private int qtdPerPage;	
 
 	@Test
 	public void contextLoads() {
-	}
-
+		assertEquals(50, qtdPerPage);	
+	}	
+	
 }
