402c53cec8d69cc98cc44875bf0d20beb96ca701
Angelo Brandao <angelobms@gmail.com>
2017-11-10 21:23:38 -0300

    initial version of the project

---
+++ src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
@@ -0,0 +1,12 @@
+package br.com.bmsti.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiExampleApplication.class, args);
+	}
+}
