60f2cc04360804688d9f49a5f8274b15f0259bbf
Angelo Brandão <angelobms@gmail.com>
2017-11-11 15:55:26 -0300

    Configured application parameters to access database.

--- src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
+++ src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
@@ -1,13 +1,28 @@
 package br.com.bmsti.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootApiExampleApplication {
 
+	@Value("${pagination_qtd_per_page}")
+	private int qtdPerPage;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiExampleApplication.class, args);
 		System.out.println("Spring Boot API Example");
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Amount of elements per page = " + this.qtdPerPage);
+		};
+		
+	}
+	
 }
