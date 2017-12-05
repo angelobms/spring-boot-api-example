c098a586b4d2b9350c9a3503c1ff6fdac2df7030
Angelo Brandão <angelobms@gmail.com>
2017-12-04 21:39:09 -0300

    Configured database mongodb.

--- src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
+++ src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import br.com.bmsti.api.documents.Client;
+import br.com.bmsti.api.repositories.ClientRepository;
 import br.com.bmsti.api.security.entities.User;
 import br.com.bmsti.api.security.enums.RoleEnum;
 import br.com.bmsti.api.security.repositories.UserRepository;
@@ -24,6 +26,9 @@ public class SpringBootApiExampleApplication {
 	@Autowired
 	private UserRepository userReposirory;
 	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
@@ -40,7 +45,27 @@ public class SpringBootApiExampleApplication {
 			admin.setPassword(PasswordUtils.generateBCrypt("123456"));
 			this.userReposirory.save(admin);*/
 			
+			clientRepository.deleteAll();
 			
+			clientRepository.save(new Client("Angelo", 21));
+			clientRepository.save(new Client("Renato", 34));
+			clientRepository.save(new Client("Marcela", 19));
+			clientRepository.save(new Client("Roberta", 39));
+			
+			System.out.println("List all with the findAll():");
+			System.out.println("---------------------------");
+			clientRepository.findAll();
+			System.out.println();
+			
+			System.out.println("Find a single clinet with the findByname('Angelo'):");
+			System.out.println("---------------------------");
+			clientRepository.findByName("Angelo");
+			System.out.println();
+			
+			System.out.println("Clints with age between 18 and 35:");
+			System.out.println("---------------------------");
+			clientRepository.findByAgeBetween(18, 35);
+			System.out.println();
 			
 		};
 	}
