62c855529f331475952994c59c16b6decc6cd7ca
Angelo Brandão <angelobms@gmail.com>
2017-12-10 14:31:58 -0300

    Removed config database mongodb.

--- src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
+++ src/main/java/br/com/bmsti/api/SpringBootApiExampleApplication.java
@@ -7,22 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
-import br.com.bmsti.api.documents.Client;
-import br.com.bmsti.api.repositories.ClientRepository;
-import br.com.bmsti.api.security.entities.User;
-import br.com.bmsti.api.security.enums.RoleEnum;
 import br.com.bmsti.api.security.repositories.UserRepository;
-import br.com.bmsti.api.utils.PasswordUtils;
 
-//@EnableCaching
+@EnableCaching
 @SpringBootApplication
 public class SpringBootApiExampleApplication {	
 	
 	@Autowired
 	private UserRepository userReposirory;
-	
-	@Autowired
-	private ClientRepository clientRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiExampleApplication.class, args);
@@ -44,28 +36,6 @@ public class SpringBootApiExampleApplication {
 			admin.setRole(RoleEnum.ROLE_ADMIN);
 			admin.setPassword(PasswordUtils.generateBCrypt("123456"));
 			this.userReposirory.save(admin);*/
-			
-			clientRepository.deleteAll();
-			
-			clientRepository.save(new Client("Angelo", 21));
-			clientRepository.save(new Client("Renato", 34));
-			clientRepository.save(new Client("Marcela", 19));
-			clientRepository.save(new Client("Roberta", 39));
-			
-			System.out.println("List all with the findAll():");
-			System.out.println("---------------------------");
-			clientRepository.findAll();
-			System.out.println();
-			
-			System.out.println("Find a single clinet with the findByname('Angelo'):");
-			System.out.println("---------------------------");
-			clientRepository.findByName("Angelo");
-			System.out.println();
-			
-			System.out.println("Clints with age between 18 and 35:");
-			System.out.println("---------------------------");
-			clientRepository.findByAgeBetween(18, 35);
-			System.out.println();
 			
 		};
 	}
